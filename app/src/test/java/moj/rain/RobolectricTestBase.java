52357dd06c79d4e7fbee730773498cb2d6bba070
Moj <moj.abubakr@theappbusiness.com>
2017-04-18 16:07:13 +0100

    Add OverviewActivityTest.

---
+++ app/src/test/java/moj/rain/RobolectricTestBase.java
@@ -0,0 +1,11 @@
+package moj.rain;
+
+
+import org.junit.runner.RunWith;
+import org.robolectric.RobolectricTestRunner;
+import org.robolectric.annotation.Config;
+
+@RunWith(RobolectricTestRunner.class)
+@Config(constants = BuildConfig.class)
+public abstract class RobolectricTestBase {
+}
--- app/src/test/java/moj/rain/weather/overview/view/OverviewActivityTest.java
+++ app/src/test/java/moj/rain/weather/overview/view/OverviewActivityTest.java
@@ -1,13 +1,55 @@
 package moj.rain.weather.overview.view;
 
 import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.robolectric.Robolectric;
+import org.robolectric.android.controller.ActivityController;
 
-import static org.junit.Assert.*;
+import moj.rain.RobolectricTestBase;
+import moj.rain.weather.overview.presenter.OverviewPresenter;
 
-public class OverviewActivityTest {
+import static org.mockito.BDDMockito.then;
+import static org.mockito.Mockito.times;
+
+public class OverviewActivityTest extends RobolectricTestBase {
+
+    @Mock
+    OverviewPresenter presenter;
+
+    ActivityController<OverviewActivity> activityController;
 
     @Test
-    public void onResume_getWeather() throws Exception {
+    public void givenActivityIsCreated_whenActivityIsResumed_thenGetWeatherShouldBeCalledOnce() throws Exception {
+        // Given
+        activityIsCreated();
+        presenterIsInjected();
 
-    
+        // When
+        activityController.resume();
+
+        // Then
+        then(presenter).should(times(1)).getWeather();
+        then(presenter).shouldHaveNoMoreInteractions();
+    }
+
+    @Test
+    public void givenActivityIsCreated_whenActivityIsNotResumed_thenGetWeatherShouldNotBeCalled() throws Exception {
+        // Given
+        activityIsCreated();
+        presenterIsInjected();
+
+        // Then
+        then(presenter).should(times(0)).getWeather();
+        then(presenter).shouldHaveNoMoreInteractions();
+    }
+
+    private void activityIsCreated() {
+        MockitoAnnotations.initMocks(this);
+        activityController = Robolectric.buildActivity(OverviewActivity.class).create();
+    }
+
+    private void presenterIsInjected() {
+        activityController.get().presenter = presenter;
+    }
 }
